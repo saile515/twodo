c4a559ecbc30b913c40d83454088457d7d80c20b
saile515 <elias.jorgensen2006@gmail.com>
2024-02-12 15:40:14 +0100

    rename and add callback type

---
+++ src/types/util.ts
@@ -0,0 +1,5 @@
+export type ArrayElement<ArrayType extends readonly unknown[]> = ArrayType extends readonly (infer ElementType)[]
+    ? ElementType
+    : never;
+
+export type Callback = (...args: any[]) => any;
