add12461f087d097a37a4073975f18d245aac21f
saile515 <elias.jorgensen2006@gmail.com>
2024-02-12 15:39:46 +0100

    add clip space to world space conversion

--- src/types/vector.ts
+++ src/types/vector.ts
@@ -1,3 +1,6 @@
+import Scene from "../scene.ts";
+import { mat3, vec2 } from "gl-matrix";
+
 export class Vector2 {
     private _x: number;
     private _y: number;
@@ -13,6 +16,29 @@ export class Vector2 {
 
     get y() {
         return this._y;
+    }
+
+    clip_space_to_world_space(scene: Scene) {
+        if (!scene.active_camera) {
+            return new Vector2(0, 0);
+        }
+
+        const [camera, camera_transform] = scene.active_camera;
+
+        const view_matrix = mat3.create();
+        mat3.invert(view_matrix, camera_transform.matrix);
+
+        const vp_matrix = mat3.create(); // View projection matrix
+        mat3.multiply(vp_matrix, camera.projection_matrix, view_matrix);
+
+        const inversed = mat3.create();
+        mat3.invert(inversed, vp_matrix);
+
+        const clip_space = vec2.fromValues(this._x, this._y);
+        const world_space = vec2.create();
+        vec2.transformMat3(world_space, clip_space, inversed);
+
+        return new Vector2(world_space[0], world_space[1]);
     }
 
     static sub(a: Vector2, b: Vector2) {
