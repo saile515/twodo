de843e6bbe790a9f3767fb8ffea6c536c4ef64f8
saile515 <elias.jorgensen2006@gmail.com>
2024-02-12 15:41:45 +0100

    add click callbacks

--- src/input/mouse.ts
+++ src/input/mouse.ts
@@ -1,8 +1,14 @@
 import { Vector2 } from "../types/vector.ts";
+import { Callback } from "../types/util.ts";
 
 export default class Mouse {
     private _position = new Vector2(0, 0);
     private _last_position: Vector2 | null = null;
+    private _callbacks = {
+        left_click: [] as Callback[],
+        right_click: [] as Callback[],
+        middle_click: [] as Callback[],
+    };
 
     constructor() {
         window.addEventListener("mousemove", (event) => {
@@ -11,6 +17,22 @@ export default class Mouse {
                 (event.offsetX / gl.canvas.width) * 2 - 1,
                 (event.offsetY / gl.canvas.height) * 2 - 1,
             );
+        });
+
+        window.addEventListener("click", (event) => {
+            switch (event.button) {
+                case 0:
+                    this._callbacks.left_click.forEach((callback) => callback());
+                    break;
+                case 1:
+                    this._callbacks.right_click.forEach((callback) => callback());
+                    break;
+                case 2:
+                    this._callbacks.middle_click.forEach((callback) => callback());
+                    break;
+                default:
+                    break;
+            }
         });
     }
 
@@ -25,5 +47,18 @@ export default class Mouse {
 
     get position() {
         return this._position;
+    }
+
+    register_callback(type: "left_click" | "right_click" | "middle_click", callback: Callback) {
+        this._callbacks[type].push(callback);
+        return callback;
+    }
+
+    unregister_callback(callback: Callback) {
+        for (let callback_type in this._callbacks) {
+            this._callbacks[callback_type as keyof typeof this._callbacks] = this._callbacks[
+                callback_type as keyof typeof this._callbacks
+            ].filter((element) => element != callback);
+        }
     }
 }
